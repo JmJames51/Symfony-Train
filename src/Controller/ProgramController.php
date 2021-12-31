9d0d542b199765cfd77e220693735fe73a32c238
James <jeanmariejames51@gmail.com>
2021-12-31 14:59:19 +0100

    fixed quest11

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,12 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
 use App\Entity\Category;
+use App\Entity\Episode;
+use App\Entity\Season;
 use App\Form\ProgramType;
+use App\Repository\EpisodeRepository;
+use App\Repository\SeasonRepository;
 use App\Service\Slugify;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\String\Slugger\SluggerInterface;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/program", name="program_")
@@ -59,16 +63,15 @@ class ProgramController extends AbstractController
                 $safeFileName = $slugger->slug($originalFileName);
                 $newFileName = $safeFileName . '-' . uniqid() . '.' . $posterFile->guessExtension();
 
-                    try { 
-                        $posterFile->move(
-                            $this->getParameter('poster_directory'),
-                            $newFileName
-                        );
-                    } catch (FileException $e) { 
+                try {
+                    $posterFile->move(
+                        $this->getParameter('poster_directory'),
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                }
 
-                    }
-
-                    $program->setPoster($newFileName);
+                $program->setPoster($newFileName);
             }
 
             $entityManager = $this->getDoctrine()->getManager();
@@ -89,29 +92,60 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @Route("/{program_id}", name="show")
+     * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"program_id" : "id"}})
      * @return Response
      */
-    public function show(int $id, Slugify $slugify): Response
+    public function show(Program $program, Slugify $slugify): Response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
 
         $categories = $this->getDoctrine()
             ->getRepository(Category::class)
             ->findAll();
 
-
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program with id : ' . $id . ' found in program\'s table.'
-            );
-        }
         return $this->render('program/show.html.twig', [
             'program' => $program,
             'categories' => $categories,
             'slug' => $slugify,
+        ]);
+    }
+
+    /**
+     * @Route("/{program_id}/season/{season_id}", name="show_season")
+     * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"program_id" : "id"}})
+     * @ParamConverter("seasons", class="App\Entity\Season", options={"mapping": {"season_id" : "id"}})
+     */
+    public function showSeason(Program $program, Season $seasons): Response
+    {
+
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render('program/season_show.html.twig', [
+            'seasons' => $seasons,
+            'program' => $program,
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/{program_id}/season/{season_id}/episode/{episode_id}", name="show_episode")
+     * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"program_id": "id"}})
+     * @ParamConverter("seasons", class="App\Entity\Season", options={"mapping": {"season_id": "id"}})
+     * @ParamConverter("episodes", class="App\Entity\Episode", options={"mapping": {"episode_id": "id"}})
+     */
+    public function showEpisode(Program $program, Season $seasons, Episode $episodes): Response
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render('program/episode_show.html.twig', [
+            'seasons' => $seasons,
+            'program' => $program,
+            'episodes' => $episodes,
+            'categories' => $categories,
         ]);
     }
 }
