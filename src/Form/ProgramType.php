fdb48649a6491238aeb0360ba64f4a8a1637e48d
James <jeanmariejames51@gmail.com>
2022-01-03 17:49:47 +0100

    Quest 16 finish

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +20,14 @@ class ProgramType extends AbstractType
             ->add('poster', FileType::class, ['mapped' => false])
             ->add('country')
             ->add('year')
-            ->add('category', null, ['choice_label' => 'name']);
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', null, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
